40e73f85e2ac4318e5f8699ccc8b72dc06d7ad33
Wiebe Rinsma <wiebe.rinsma@han.nl>
2025-10-01 08:38:38 +0200

    Formatting 130 chars

--- src/main/java/han/aim/se/noyoumaynot/movie/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/han/aim/se/noyoumaynot/movie/controller/RestResponseEntityExceptionHandler.java
@@ -10,30 +10,19 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler
-        extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(value
-            = { AuthenticationException.class })
-    protected ResponseEntity<Object> handleUnauthenticated(
-            RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = { AuthenticationException.class })
+    protected ResponseEntity<Object> handleUnauthenticated(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 
-    @ExceptionHandler(value
-            = { AuthorizationException.class })
-    protected ResponseEntity<Object> handleUnauthorized(
-            RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.FORBIDDEN, request);
+    @ExceptionHandler(value = { AuthorizationException.class })
+    protected ResponseEntity<Object> handleUnauthorized(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.FORBIDDEN, request);
     }
 
-    @ExceptionHandler(value
-            = { MovieNotFoundException.class })
-    protected ResponseEntity<Object> handleNotFound(
-            RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    @ExceptionHandler(value = { MovieNotFoundException.class })
+    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 }
